61451ba259152820069456422f6933a5b92a278c
Dinuka Jayamuni <A144280@agl.com.au>
2020-08-31 09:41:31 +1000

    Ready unit tests

--- src/Calculator/Calculator.Tests/Steps/CalculatorSteps.cs
+++ src/Calculator/Calculator.Tests/Steps/CalculatorSteps.cs
@@ -131,6 +131,12 @@ namespace Calculator.Tests.Steps
             await ExecuteOperation(OperationType.SimpleDivision);
         }
 
+        [When(@"the first number is to the power of second number")]
+        public async Task WhenTheFirstNumberIsToThePowerOfSecondNumber()
+        {
+            await ExecuteOperation(OperationType.SimpleExponential);
+        }
+
         [Then(@"the result should be (.*)")]
         public void ThenTheResultShouldBe(decimal result)
         {
